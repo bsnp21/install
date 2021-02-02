4885ae51fd761eae5f41939a561d5ca525c0e220
Ubuntu <ubuntu@ip-172-31-87-112.ec2.internal>
2021-02-02 23:57:58 +0000

    cmd

--- cmd/setup_sum.sh
+++ cmd/setup_sum.sh
@@ -2,14 +2,14 @@
 
 
 
-setup_apache.sh
+./setup_apache.sh
 
 
-setup_app_base.sh
+./setup_app_base.sh
 
-setup_app_mkdir.sh
+./setup_app_mkdir.sh
 
-setup_app_update_github_home.sh
+./setup_app_update_github_home.sh
 
 
-setup_pm2_auto_restart.sh
+./setup_pm2_auto_restart.sh
