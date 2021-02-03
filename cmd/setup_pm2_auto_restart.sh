f8fcff0bd6942a3da550544bc51f7ae6369b31d2
wdingsoft <wdingbox@gmail.com>
2021-02-02 19:10:12 -0500

    adj

--- cmd/setup_pm2_auto_restart.sh
+++ cmd/setup_pm2_auto_restart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-cd /var/www/html/wdawd
+cd /var/www/html/wdaws/ham12/utility/ndjs/weindjs_restapi
 
 pm2 startup
 #
@@ -14,7 +14,7 @@ sudo env PATH=$PATH:/usr/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup sys
 
 pm2 startup upstart
 
-pm2 start /var/www/html/wdaws/ham12/utility/ndjs/weindjs_restapi/a.node.js
+pm2 start a.node.js
 
 pm2 save
 
