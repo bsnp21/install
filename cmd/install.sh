17675e011534726c5a56c83b480d9d4102cecb74
wdingsoft <wdingbox@gmail.com>
2021-02-04 14:01:39 -0500

    remove before install

--- cmd/install.sh
+++ cmd/install.sh
@@ -2,19 +2,17 @@
 
 
 
+
+sudo rm -rf /var/www/html/wdaws
+
 ./setup_apache.sh
-
-
 ./setup_app_base.sh
-
 ./setup_app_mkdir.sh
-
-#./update_github_home_svip.sh
-
 
 ./setup_pm2_auto_restart.sh
 
 
 
+#./update_github_home_svip.sh
 SvrIP=`dig +short myip.opendns.com @resolver1.opendns.com`
 echo SvrIP=${SvrIP}
