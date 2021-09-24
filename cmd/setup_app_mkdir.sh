7a28615e9fbc795c906643258065e72a296b8631
wdingsoft <wdingbox@gmail.com>
2021-09-24 14:24:45 -0400

    sh

--- cmd/setup_app_mkdir.sh
+++ cmd/setup_app_mkdir.sh
@@ -25,25 +25,6 @@ sudo ./install_node_modules.sh
 
 
 #############
-pm2 delete 0
-
-pm2 startup
-#
-#  [PM2] Init System found: systemd
-#  [PM2] To setup the Startup Script, copy/paste the following command:
-#  sudo env PATH=$PATH:/usr/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
-
-
-sudo env PATH=$PATH:/usr/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
-
-pm2 startup upstart
-
-pm2 start a.node.js
-
-pm2 save
-
-
-pm2 list
 
 
 
