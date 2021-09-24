e7a9963cb34594768d543c8102a4a2995caf9d7f
wdingsoft <wdingbox@gmail.com>
2021-09-24 13:59:04 -0400

    update sh

--- cmd/setup_app_mkdir.sh
+++ cmd/setup_app_mkdir.sh
@@ -4,21 +4,20 @@
 cd /var/www/html
 sudo mkdir wdaws
 cd wdaws
-sudo git clone https://github.com/wdingbox/ham12.git
+
+#base libs.
 sudo git clone https://github.com/wdingbox/bible_concordance.git
 sudo git clone https://github.com/wdingbox/bible_obj_lib.git
-sudo git clone https://github.com/bsnp21/install.git
+#sudo git clone https://github.com/bsnp21/install.git
 #git clone https://github.com/wdingbox/bible_obj_usr.git
 #git clone https://github.com/wdingbox/hebrew_ciu.git
 
+#Client-Svr
+sudo git clone https://github.com/bsnp21/bb.git
+#sudo cp ./ham12/utility/wdaws/*.htm ../index.html
 
-sudo cp ./ham12/utility/wdaws/*.htm ../index.html
 
+cd bb/svr/restapi
+sudo ./install_node_modules.sh
 
-cd ham12/utility/ndjs/weindjs_restapi/
-sudo npm i express
-sudo npm i cors
-sudo npm i node-cache
-sudo npm i cheerio
-sudo npm i express-fileupload
 
