9a097f3880a5be7542096052271e0b82f91c4113
wdingsoft <wdingbox@gmail.com>
2021-09-24 15:37:02 -0400

    sh pm2 start

--- cmd/setup_pm2_auto_restart.sh
+++ cmd/setup_pm2_auto_restart.sh
@@ -3,9 +3,9 @@
 
 cd /var/www/html/wdaws/bb/svr/restapi
 
-pm2 delete 0
+#pm2 delete 0
 
-pm2 startup
+#pm2 startup
 #
 #  [PM2] Init System found: systemd
 #  [PM2] To setup the Startup Script, copy/paste the following command:
@@ -14,7 +14,7 @@ pm2 startup
 
 sudo env PATH=$PATH:/usr/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
 
-pm2 startup upstart
+#pm2 startup upstart
 
 pm2 start a.node.js
 
