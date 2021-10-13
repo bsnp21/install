c75cb4f87d4cc5ec85800169e744ec359bc10a36
wdingsoft <wdingbox@gmail.com>
2021-10-13 12:44:16 -0400

    install https

--- apache2_conf/setup_https.sh
+++ apache2_conf/setup_https.sh
@@ -1,8 +1,18 @@
 #/bin/
 
+#  https://techexpert.tips/apache/enable-https-apache/
+
+apt-get update
+apt-get install apache2 openssl
+
+#####
+a2enmod ssl
+a2enmod rewrite
+
+
+
 
 cp -fv 000-default.conf.after /etc/apache2/sites-enabled/000-default.conf
-
 cp -fv apache2.conf /etc/apache2/apache2.conf
 
 
