e0a6f441528dba18c33c34a9db808f1f612233bc
wdingsoft <wdingbox@gmail.com>
2021-10-15 09:14:40 -0400

    ename

---
+++ apache2_https/setup_obiLab.sh
@@ -0,0 +1,46 @@
+
+
+HOSTPATH="/var/www/html"
+WKDIR="obi/data"
+ 
+cd ${HOSTPATH}
+
+rm -rf obi
+mkdir -p ${WKDIR}
+cd ${WKDIR}
+touch index.html
+
+USR1="wding"
+USR2="soft"
+PWD1="ghp"
+PWD2="bmS4tYs3eC"
+PWD3="OpnDUoS1Yd"
+PWD4="O49UqRfAUX42pD4y"
+PROJ1="obimg"
+PROJ2="obronze"
+GITPATH1="github.com/${USR1}${USR2}/${PROJ1}.git"
+GITPATH2="github.com/${USR1}${USR2}/${PROJ2}.git"
+#base libs.
+
+
+echo git clone  https://${USR1}${USR2}:${PWD1}_${PWD2}${PWD3}${PWD4}@${GITPATH1}
+sudo git clone  https://${USR1}${USR2}:${PWD1}_${PWD2}${PWD3}${PWD4}@${GITPATH1}
+
+echo rm -rf ${WKDIR}/${PROJ1}/.git
+sudo rm -rf ${PROJ1}/.git
+
+echo git clone https://${USR1}${USR2}:${PWD1}_${PWD2}${PWD3}${PWD4}@${GITPATH2}
+sudo git clone https://${USR1}${USR2}:${PWD1}_${PWD2}${PWD3}${PWD4}@${GITPATH2}
+
+echo rm -rf ${WKDIR}/${PROJ2}/.git
+sudo rm -rf ${PROJ2}/.git
+
+
+
+
+
+
+#############
+
+
+
