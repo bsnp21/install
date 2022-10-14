4c5b326563cf88157e19cc73c2b35745cb4bc945
wdingsoft <wdingbox@gmail.com>
2022-10-14 12:12:11 -0400

    -

--- cron_method/enable_autostart.sh
+++ cron_method/enable_autostart.sh
@@ -3,13 +3,13 @@
 APP_PATH="/var/www/html/wdaws/bb/svr/restapi"
 APP_NODE="${APP_PATH}/a.node.js"
 
-EXE_CMD="@reboot sleep 300 && node ${APP_NODE}"
+EXE_CMD="@reboot sleep 300 && node ${APP_NODE} > ~/a.nod.log"
 
 echo ${EXE_CMD} 
 
 ### 
 
-(crontab -l 2>/dev/null || true; echo "echo test_start >>test_start.log") | crontab -
+(crontab -l 2>/dev/null || true; echo "* * * * * echo test_start >>test_start.log") | crontab -
 
 
 
@@ -17,7 +17,7 @@ echo ${EXE_CMD}
 
 
 
-(crontab -l 2>/dev/null || true; echo "echo test_end >>crontab_test_end.log") | crontab -
+(crontab -l 2>/dev/null || true; echo "* * * * * echo test_start >>test_start.log") | crontab -
 
 
 
