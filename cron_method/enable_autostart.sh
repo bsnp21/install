9c96d06484b974f5b7e46d760ca0f797e5c0318d
wdingsoft <wdingbox@gmail.com>
2022-10-14 11:50:08 -0400

    modified:   enable_autostart.sh

--- cron_method/enable_autostart.sh
+++ cron_method/enable_autostart.sh
@@ -5,12 +5,17 @@ APP_NODE="${APP_PATH}/a.node.js"
 
 EXE_CMD="@reboot sleep 300 && node ${APP_NODE}"
 
-echo ${EXE_CMD}
+echo ${EXE_CMD} 
 
 ### 
+
+(crontab -l 2>/dev/null || true; echo "echo test_start >>test_start.log") | crontab -
+
+
+
 (crontab -l 2>/dev/null || true; echo "${EXE_CMD}") | crontab -
 
 
 
-(crontab -l 2>/dev/null || true; echo "*/5 * * * * /path/to/job -with args") | crontab -
+(crontab -l 2>/dev/null || true; echo "echo test_end >>crontab_test_end.log") | crontab -
 
