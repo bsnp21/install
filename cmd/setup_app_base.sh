d46690df68688b11fe048e9cfb43c6c55bd7efbb
wdingsoft <wdingbox@gmail.com>
2022-10-25 08:49:26 -0400

    new file:   ../.gitignore modified:   setup_app_base.sh

--- cmd/setup_app_base.sh
+++ cmd/setup_app_base.sh
@@ -6,6 +6,8 @@ dig +short myip.opendns.com @resolver1.opendns.com
 
 sudo apt-get update
 
+npm install -g n
+
 sudo apt-get install git
 
 sudo apt-get install npm
@@ -21,3 +23,22 @@ sudo pm2 unstartup
 sudo pm2 startup
 
 
+
+sudo npm --version
+# 8.19.2
+
+node --version
+#v10.19.0
+
+pm2 --version
+# [PM2] Spawning PM2 daemon with pm2_home=/home/ubuntu/.pm2
+# [PM2] PM2 Successfully daemonized
+# 5.2.2
+
+
+# npm ERR! code EBADENGINE
+# npm ERR! engine Unsupported engine
+# npm ERR! engine Not compatible with your version of node/npm: npm@8.19.2
+# npm ERR! notsup Not compatible with your version of node/npm: npm@8.19.2
+# npm ERR! notsup Required: {"node":"^12.13.0 || ^14.15.0 || >=16"}
+# npm ERR! notsup Actual:   {"npm":"7.24.2","node":"v10.19.0"}
