815d6a3d03efb5233d315369e7d6ee51eb11f2e4
wdingsoft <wdingbox@gmail.com>
2022-11-08 11:46:32 -0500

    npm node pm2

---
+++ cmd/setup_pm2.sh
@@ -0,0 +1,18 @@
+
+sudo apt-get install npm
+
+# ubuntu@ip-172-31-87-112:~/install/cmd$ npm -v
+# 6.14.4
+# ubuntu@ip-172-31-87-112:~/install/cmd$ 
+# ubuntu@ip-172-31-87-112:~/install/cmd$ 
+# ubuntu@ip-172-31-87-112:~/install/cmd$ node -v
+# v10.19.0
+# ubuntu@ip-172-31-87-112:~/install/cmd$ 
+
+
+
+
+
+sudo npm install pm2 -g
+# sudo npm install pm2@latest -g
+# sudo pm2 update
