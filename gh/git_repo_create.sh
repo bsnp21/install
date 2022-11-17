4219739b55e6d6647cb4035b7d3c105276a2a8de
wdingsoft <wdingbox@gmail.com>
2022-11-17 18:49:01 -0500

    chmod

--- gh/git_repo_create.sh
+++ gh/git_repo_create.sh
@@ -1,8 +1,8 @@
 # create my-project and clone 
 # gh repo create gh001 --public --clone
 
-mkdir test_gh
-cd test_gh
+mkdir git001
+cd git001
 
 #or create a new repository on the command line
 echo "# gh001" >> README.md
@@ -10,6 +10,6 @@ git init
 git add README.md
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/bsnp21/gh001.git
+git remote add origin https://github.com/bsnp21/git001.git
 git push -u origin main
 
