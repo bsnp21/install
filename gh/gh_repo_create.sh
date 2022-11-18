25fb1f198ff7daea6b0595806546a7c2cc6f460a
wdingsoft <wdingbox@gmail.com>
2022-11-17 19:03:53 -0500

    rename

--- gh/gh_repo_create.sh
+++ gh/gh_repo_create.sh
@@ -1,2 +1,11 @@
 # create my-project and clone 
-gh repo create gh001 --public --clone
+echo $1
+gh repo create $1 --public --clone
+
+cd $1
+echo $1 > readme.txt
+git add *
+git commit -m "readme.txt"
+git branch -M main
+git remote add origin https://github.com/bsnp21/git001.git
+git push -u origin main
