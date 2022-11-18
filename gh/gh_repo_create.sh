08bd423a6766369ffb8c1bcb3c6a495b73ba374c
wdingsoft <wdingbox@gmail.com>
2022-11-17 19:04:18 -0500

    rename

--- gh/gh_repo_create.sh
+++ gh/gh_repo_create.sh
@@ -7,5 +7,5 @@ echo $1 > readme.txt
 git add *
 git commit -m "readme.txt"
 git branch -M main
-git remote add origin https://github.com/bsnp21/git001.git
+git remote add origin https://github.com/bsnp21/$1.git
 git push -u origin main
