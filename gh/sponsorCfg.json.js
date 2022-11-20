1ac4cd5deae2f1f4ac0a4c4d3115e2428bf1c828
wdingsoft <wdingbox@gmail.com>
2022-11-20 05:52:19 -0500

    JSON str

--- gh/sponsorCfg.json.js
+++ gh/sponsorCfg.json.js
@@ -1,4 +1,4 @@
-var sponsorCfg={
-    "ownername":"YnNucGdocmVwb2xpc3Q=",
-    "ownerpets":["Z2hwX00wbVJ1SVE0bk5WenEz","SVFwTVdLN2txNlhyaFM5eTRkMU5OUw=="]
+var sponsorCfg = {
+    "ownername": "YnNucGdocmVwb2xpc3Q=",
+    "ownerpets": ["Z2hwX00wbVJ1SVE0bk5WenEz", "SVFwTVdLN2txNlhyaFM5eTRkMU5OUw=="]
 }
