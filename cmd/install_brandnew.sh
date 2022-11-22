285cca465dae7fcca72cf74e16af949f5cca85b9
wdingsoft <wdingbox@gmail.com>
2022-11-22 11:23:09 -0500

    rename

--- cmd/install_brandnew.sh
+++ cmd/install_brandnew.sh
@@ -4,16 +4,19 @@
 #sudo git clone https://github.com/bsnp21/install.git 
 
 
-./stop_pm2.sh
-./uninstall_pm2.sh
+#./stop_pm2.sh
+#./uninstall_pm2.sh
 
-sudo rm -rf /var/www/html/wdaws
+#sudo rm -rf /var/www/html/wdaws
 
 ./setup_apache.sh
-./setup_npm.sh
-./setup_user_app.sh
+./setup_git.sh
+./setup_npm_node.sh
+./setup_proj_host.sh
+./setup_proj_obi.sh
+./setup_proj_system_service.sh
 
-#./start_pm2.sh
+
 
 
 
---
+++ cmd/install_refresh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+#sudo git clone https://github.com/bsnp21/install.git 
+
+
+
+sudo rm -rf /var/www/html/wdaws
+
+
+./setup_proj_host.sh
+./setup_proj_obi.sh
+
