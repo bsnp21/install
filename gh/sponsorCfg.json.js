d067ef540a3e545a75e0c186dbdf89569423aa1d
wdingsoft <wdingbox@gmail.com>
2022-11-22 16:43:23 -0500

    -

--- gh/sponsorCfg.json.js
+++ gh/sponsorCfg.json.js
@@ -1,4 +1,4 @@
 var sponsorCfg = {
     "ownername": "YnNucGdocmVwb2xpc3Q=",
-    "ownerpets": ["Z2hwXz"+"k2Z"+"HZZO"+"ENKUn"+"lCR"+"VhLOX"+"NJZmZJ"+"S1htSD"+"Vue"+"WREZT"+"FENnI0TA"+"=="]
+    "ownerpets": ["Z2hwXz", "k2Z", "HZZO", "ENKUn", "lCR", "VhLOX", "NJZmZJ", "S1htSD", "Vue", "WREZT", "FENnI0TA", "=="]
 }
