836718c1d56a0c836c98acfdfc6b89fbad929f5d
wdingsoft <wdingbox@gmail.com>
2022-11-22 11:12:26 -0500

    cmd reorg

--- cmd/setup_visually_gh.sh
+++ cmd/setup_visually_gh.sh
@@ -1,15 +1,7 @@
 
 
 
-sudo apt-get install git
-
-git config --list --show-origin
-
-git remote -v
-
-git config --global --add safe.directory '*'
-git config --global init.defaultBranch main
-
+../gh/setup.sh
 #############
 
 
