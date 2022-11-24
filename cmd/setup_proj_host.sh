5c3bb9d760f6847a8ce2a8076cee71246b7e4a26
wdingsoft <wdingbox@gmail.com>
2022-11-24 18:09:03 -0500

    remove sudo in setup

--- cmd/setup_proj_host.sh
+++ cmd/setup_proj_host.sh
@@ -3,19 +3,19 @@
 
 cd /var/www/html
 sudo rm -rf wdaws
-sudo mkdir wdaws
+mkdir wdaws
 cd wdaws
 
 #base libs.
-sudo git clone https://github.com/wdingbox/bible_concordance.git
-sudo git clone https://github.com/wdingbox/bible_obj_lib.git
+git clone https://github.com/wdingbox/bible_concordance.git
+git clone https://github.com/wdingbox/bible_obj_lib.git
 #sudo git clone https://github.com/bsnp21/install.git
 #git clone https://github.com/wdingbox/bible_obj_usr.git
 #git clone https://github.com/wdingbox/hebrew_ciu.git
 
 #Client-Svr
-sudo git clone https://github.com/bsnp21/home.git
-sudo git clone https://github.com/bsnp21/bb.git
+git clone https://github.com/bsnp21/home.git
+git clone https://github.com/bsnp21/bb.git
 #sudo cp ./ham12/utility/wdaws/*.htm ../index.html
 
 
