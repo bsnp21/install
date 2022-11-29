d7d50da6cb1193f8f1e464424278db0aac1e2f84
wdingsoft <wdingbox@gmail.com>
2022-11-28 20:17:29 -0500

    gh-pages

--- gh/_gh_pages_run_deploy.sh
+++ gh/_gh_pages_run_deploy.sh
@@ -1,11 +1,18 @@
 
 
+
 echo reponame=$1
+
+git clone https://github.com/bsnpghrepolist/$1.git
+
 cd $1
-echo "./myoj/e_Note(e_Note.json)">account/README.md
+echo "[./myoj/e_Note](./myoj/e_Note.json)">account/README.md
 echo node_modules > .gitignore
 npm i gh-pages
 npm init -y 
 # modify  package.script in package.json
 sudo vi package.json #add "deploy":"gh-pages -d account"
 npm run deploy
+
+#published site:
+echo https://bsnpghrepolist.github.io/$1/myoj/e_Note_json.js
