b399541daf0f1fe1ed6c6fc50effbbdedf472049
wdingsoft <wdingbox@gmail.com>
2022-11-30 14:08:23 -0500

    try new git create

---
+++ gh/_git_init_create_new.sh
@@ -0,0 +1,15 @@
+# create my-project and clone 
+# gh repo create gh001 --public --clone
+
+mkdir $1
+cd $1
+
+#or create a new repository on the command line
+echo "# gh001" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/bsnpghrepolist/$1.git
+git push -u origin main
+
