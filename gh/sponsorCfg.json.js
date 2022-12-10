951dd86261c9507836f9d850040442a9d81e6b96
wdingsoft <wdingbox@gmail.com>
2022-12-10 04:54:19 -0500

    up

--- gh/sponsorCfg.json.js
+++ gh/sponsorCfg.json.js
@@ -1,4 +1,7 @@
 var sponsorCfg = {
     "ownername": "YnNucGdocmVwb2xpc3Q=",
-    "ownerpets": ["Z2hwXz", "k2Z", "HZZO", "ENKUn", "lCR", "VhLOX", "NJZmZJ", "S1htSD", "Vue", "WREZT", "FENnI0TA", "=="]
+    "ownerpet1": ["Z2hwXz", "k2Z", "HZZO", "ENKUn", "lCR", "VhLOX", "NJZmZJ", "S1htSD", "Vue", "WREZT", "FENnI0TA", "=="],
+    "ownerpet2": ["Z2hwXz", "k2Z", "HZO", "EKUn", "lCR", "VhLOX", "NJZmZJ", "S1hSD", "Vue", "WREZT", "FENnI0TA", "=="],
+    "ownerpet3": ["2hwXz", "k2", "HZZO", "ENKUn", "lCR", "VLOX", "NJZmZJ", "S1htSD", "Vue", "WREZT", "FENnI0TA", "=="],
+    "ownerpets": ["Z2hwX", "3dMTlBUbFh", "Dclo5NmNlYTMwVlpERzl", "RVWFCaVAwejJxNUcwQg", "=="]
 }
