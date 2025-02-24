cbfaeb96a2f4465b937ea63f6b123d35f6505a53
wdingsoft <wdingbox@gmail.com>
2025-02-24 04:13:57 -0500

    clean

--- cmd/update_github_home_svc_ip.sh
+++ cmd/update_github_home_svc_ip.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-cd ~
-git clone https://github.com/bsnp21/home.git
-
-cd home/js
-
-FileName="InstalledDeviceIP.js"
-echo " InstalledDeviceIP = '$1'" > ${FileName}
-# git add *
-# git commit -m "'$1'"
-# git push
-FileName="InstalledDeviceIP.js"
-
-echo " InstalledDeviceIP = '$1'" > ${FileName}
-
-
-git add *
-git commit -m "update ip $1 "
-git push
-
-cd ~
+##  cd ~
+##  git clone https://github.com/bsnp21/home.git
+##  
+##  cd home/js
+##  
+##  FileName="InstalledDeviceIP.js"
+##  echo " InstalledDeviceIP = '$1'" > ${FileName}
+##  # git add *
+##  # git commit -m "'$1'"
+##  # git push
+##  FileName="InstalledDeviceIP.js"
+##  
+##  echo " InstalledDeviceIP = '$1'" > ${FileName}
+##  
+##  
+##  git add *
+##  git commit -m "update ip $1 "
+##  git push
+##  
+##  cd ~
 
